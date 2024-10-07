2de6345476d3eba1cd03001ba54bcde8207b3500
Emmanuel Joshua <emiranojoshua@gmail.com>
2024-10-07 05:56:16 +0100

    day 10 laravel tutorial

--- emirano/routes/web.php
+++ emirano/routes/web.php
@@ -3,9 +3,16 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Mail\JobPosted;
 use App\Models\Job;
 // use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
+Route::get("/test", function () {
+   
+});
+
 
 //home route
 Route::view('/', 'home');
