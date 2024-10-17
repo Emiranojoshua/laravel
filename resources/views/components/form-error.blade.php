1e5edbc5149db0f8544614a9c9d81be4cb5609cf
Emmanuel Joshua <emiranojoshua@gmail.com>
2024-10-17 09:52:34 +0100

    Initial commit from emirano

---
+++ resources/views/components/form-error.blade.php
@@ -0,0 +1,5 @@
+@props(['name'])
+
+@error($name)
+   {{$message}}<br><br>
+@enderror
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layouts>
+    <x-slot:heading>
+        <h1>contact page</h1>
+    </x-slot:heading>
+</x-layouts>
